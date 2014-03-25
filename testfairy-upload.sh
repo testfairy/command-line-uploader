6b3b935800e7e0556cbff7b2324f79707f55a8cf
TestFairy <root@goliath.(none)>
2014-03-25 15:33:22 +0200

    fixed location of temporary files removed requirement of SSLv3 in CURL, fixes on cygwin support for spaces in filenames using SHA1 digest for backward compatibility with older Android devices validating parameters before execution validating tools are available before execution other bugfixes

--- testfairy-upload.sh
+++ testfairy-upload.sh
@@ -14,38 +14,100 @@ ALIAS=
 # locations of various tools
 CURL=curl
 ZIP=zip
+KEYTOOL=keytool
 ZIPALIGN=zipalign
 JARSIGNER=jarsigner
+
 SERVER_ENDPOINT=https://app.testfairy.com
 
-if [ $# -ne 1 ]; then
+usage() {
 	echo "Usage: testfairy-upload.sh APK_FILENAME"
 	echo
+}
+	
+verify_tools() {
+
+	# Check 'zip' tool
+	${ZIP} -h >/dev/null
+	if [ $? -ne 0 ]; then
+		echo "Could not run zip tool, please check settings"
+		exit 1
+	fi
+	
+	# Check 'curl' tool
+	${CURL} --help >/dev/null
+	if [ $? -ne 0 ]; then
+		echo "Could not run curl tool, please check settings"
+		exit 1
+	fi
+	
+	OUTPUT=$( ${JARSIGNER} -help 2>&1 )
+	if [ $? -ne 0 ]; then
+		echo "Could not run jarsigner tool, please check settings"
+		exit 1
+	fi
+	
+	# Check 'zipalign' tool
+	OUTPUT=$( ${ZIPALIGN} 2>&1 | grep -i "Zip alignment" )
+	if [ $? -ne 0 ]; then
+		echo "Could not run zipalign tool, please check settings"
+		exit 1
+	fi
+
+	OUTPUT=$( ${KEYTOOL} -help 2>&1 )
+	if [ $? -ne 0 ]; then
+		echo "Could not run keytool tool, please check settings"
+		exit 1
+	fi
+}
+
+verify_settings() {
+	if [ -z "${TESTFAIRY_API_KEY}" ]; then
+		usage
+		echo "Please update API_KEY with your private API key, as noted in the Settings page"
+		exit 1
+	fi
+
+	if [ -z "${KEYSTORE}" -o -z "${STOREPASS}" -o -z "{$ALIAS}" ]; then
+		usage
+		echo "Please update KEYSTORE, STOREPASS and ALIAS with your jar signing credentials"
+		exit 1
+	fi
+
+	# verify KEYSTORE, STOREPASS and ALIAS at once
+	OUTPUT=$( ${KEYTOOL} -list -keystore "${KEYSTORE}" -storepass "${STOREPASS}" -alias "${ALIAS}" 2>&1 )
+	if [ $? -ne 0 ]; then
+		usage
+		echo "Please check keystore credentials; keytool failed to verify storepass and alias"
+		exit 1
+	fi
+}
+
+if [ $# -ne 1 ]; then
+	usage
 	exit 1
 fi
 
+# before even going on, make sure all tools work
+verify_tools
+verify_settings
+
 APK_FILENAME=$1
 if [ ! -f "${APK_FILENAME}" ]; then
-	echo "Usage: testfairy-upload.sh APK_FILENAME"
-	echo
+	usage
 	echo "Can't find file: ${APK_FILENAME}"
 	exit 2
 fi
 
-TMP_FILENAME=/tmp/.testfairy.upload.apk
-ZIPALIGNED_FILENAME=/tmp/.testfairy.zipalign.apk
-
-if [ -z "${TESTFAIRY_API_KEY}" ]; then
-	echo "Usage: testfairy-upload.sh APK_FILENAME"
-	echo
-	echo "Please update API_KEY with your private API key, as noted in the Settings page"
-	exit 1
-fi
+# temporary file paths
+TMP_FILENAME=.testfairy.upload.apk
+ZIPALIGNED_FILENAME=.testfairy.zipalign.apk
+rm -f "${TMP_FILENAME}" "${ZIPALIGNED_FILENAME}"
 
 /bin/echo -n "Uploading ${APK_FILENAME} to TestFairy.. "
-JSON=`${CURL} -3s ${SERVER_ENDPOINT}/api/upload -F api_key=${TESTFAIRY_API_KEY} -F apk_file=@${APK_FILENAME}`
+JSON=$( ${CURL} -s ${SERVER_ENDPOINT}/api/upload -F api_key=${TESTFAIRY_API_KEY} -F apk_file=@${APK_FILENAME} )
 
-URL=`echo ${JSON} | grep -o '"instrumented_url"\s*:\s*"[^"]*"' | sed 's/"instrumented_url"\s*:\s*"\([^"]*\)"/\1/' | sed 's/\\\//g'`
+URL=$( echo ${JSON} | sed 's/\\\//\//g' | sed -n 's/.*"instrumented_url"\s*:\s*"\([^"]*\)".*/\1/p' )
 if [ -z "${URL}" ]; then
 	echo "FAILED!"
 	echo 
@@ -54,13 +116,13 @@ if [ -z "${URL}" ]; then
 fi
 
 echo "OK!"
-/bin/echo -n "Downloading instrumented APK from ${URL}.. "
-${CURL} -o ${TMP_FILENAME} -3s ${URL}
+/bin/echo -n "Downloading instrumented APK.. "
+${CURL} -o ${TMP_FILENAME} -s ${URL}
 echo "OK!"
 
 /bin/echo -n "Re-signing APK file.. "
-${ZIP} -qd ${TMP_FILENAME} META-INF/\*
-${JARSIGNER} -keystore ${KEYSTORE} -storepass ${STOREPASS} ${TMP_FILENAME} ${ALIAS}
+${ZIP} -qd ${TMP_FILENAME} 'META-INF/*'
+${JARSIGNER} -keystore "${KEYSTORE}" -storepass "${STOREPASS}" -digestalg SHA1 -sigalg MD5withRSA ${TMP_FILENAME} "${ALIAS}"
 ${JARSIGNER} -verify ${TMP_FILENAME} >/dev/null
 if [ $? -ne 0 ]; then
 	echo "FAILED!"
@@ -74,8 +136,18 @@ rm -f ${TMP_FILENAME}
 echo "OK!"
 
 /bin/echo -n "Uploading signed APK to TestFairy.. "
-${CURL} -3s ${SERVER_ENDPOINT}/api/upload-signed -F api_key=${TESTFAIRY_API_KEY} -F apk_file=@${ZIPALIGNED_FILENAME} -F testers_groups=${TESTER_GROUPS}
-echo "OK!"
-
+JSON=$( ${CURL} -s ${SERVER_ENDPOINT}/api/upload-signed -F api_key=${TESTFAIRY_API_KEY} -F apk_file=@${ZIPALIGNED_FILENAME} -F testers-groups="${TESTER_GROUPS}" )
 rm -f ${ZIPALIGNED_FILENAME}
 
+URL=$( echo ${JSON} | sed 's/\\\//\//g' | sed -n 's/.*"build_url"\s*:\s*"\([^"]*\)".*/\1/p' )
+if [ -z "$URL" ]; then
+	echo "FAILED!"
+	echo
+	echo "Build uploaded, but no reply from server. Please contact support@testfairy.com"
+	exit 1
+fi
+
+echo "OK!"
+echo
+echo "Build was successfully uploaded to TestFairy and is available at:"
+echo ${URL}
