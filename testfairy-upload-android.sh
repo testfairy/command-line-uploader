1db6ebcd00faefa3e7468f28c90653339cf7a788
Gil Megidish <gil@megidish.net>
2015-11-17 12:46:17 +0200

    Added quotes for Windows support ("Program Files (x86)")

--- testfairy-upload-android.sh
+++ testfairy-upload-android.sh
@@ -51,33 +51,33 @@ verify_tools() {
 	# Windows users: this script requires zip, curl and sed. If not installed please get from http://cygwin.com/
 	
 	# Check 'zip' tool
-	${ZIP} -h >/dev/null
+	"${ZIP}" -h >/dev/null
 	if [ $? -ne 0 ]; then
 		echo "Could not run zip tool, please check settings"
 		exit 1
 	fi
 	
 	# Check 'curl' tool
-	${CURL} --help >/dev/null
+	"${CURL}" --help >/dev/null
 	if [ $? -ne 0 ]; then
 		echo "Could not run curl tool, please check settings"
 		exit 1
 	fi
 	
-	OUTPUT=$( ${JARSIGNER} -help 2>&1 | grep "verify" )
+	OUTPUT=$( "${JARSIGNER}" -help 2>&1 | grep "verify" )
 	if [ $? -ne 0 ]; then
 		echo "Could not run jarsigner tool, please check settings"
 		exit 1
 	fi
 	
 	# Check 'zipalign' tool
-	OUTPUT=$( ${ZIPALIGN} 2>&1 | grep -i "Zip alignment" )
+	OUTPUT=$( "${ZIPALIGN}" 2>&1 | grep -i "Zip alignment" )
 	if [ $? -ne 0 ]; then
 		echo "Could not run zipalign tool, please check settings"
 		exit 1
 	fi
 
-	OUTPUT=$( ${KEYTOOL} -help 2>&1 | grep "keypasswd" )
+	OUTPUT=$( "${KEYTOOL}" -help 2>&1 | grep "keypasswd" )
 	if [ $? -ne 0 ]; then
 		echo "Could not run keytool tool, please check settings"
 		exit 1
@@ -98,7 +98,7 @@ verify_settings() {
 	fi
 
 	# verify KEYSTORE, STOREPASS and ALIAS at once
-	OUTPUT=$( ${KEYTOOL} -list -keystore "${KEYSTORE}" -storepass "${STOREPASS}" -alias "${ALIAS}" 2>&1 )
+	OUTPUT=$( "${KEYTOOL}" -list -keystore "${KEYSTORE}" -storepass "${STOREPASS}" -alias "${ALIAS}" 2>&1 )
 	if [ $? -ne 0 ]; then
 		usage
 		echo "Please check keystore credentials; keytool failed to verify storepass and alias"
@@ -129,7 +129,7 @@ ZIPALIGNED_FILENAME=.testfairy.zipalign.apk
 rm -f "${TMP_FILENAME}" "${ZIPALIGNED_FILENAME}"
 
 /bin/echo -n "Uploading ${APK_FILENAME} to TestFairy.. "
-JSON=$( ${CURL} -s ${SERVER_ENDPOINT}/api/upload -F api_key=${TESTFAIRY_API_KEY} -F apk_file="@${APK_FILENAME}" -F icon-watermark="${ICON_WATERMARK}" -F video="${VIDEO}" -F max-duration="${MAX_DURATION}" -F comment="${COMMENT}" -A "TestFairy Command Line Uploader ${UPLOADER_VERSION}" )
+JSON=$( "${CURL}" -s ${SERVER_ENDPOINT}/api/upload -F api_key=${TESTFAIRY_API_KEY} -F apk_file="@${APK_FILENAME}" -F icon-watermark="${ICON_WATERMARK}" -F video="${VIDEO}" -F max-duration="${MAX_DURATION}" -F comment="${COMMENT}" -A "TestFairy Command Line Uploader ${UPLOADER_VERSION}" )
 
 URL=$( echo ${JSON} | sed 's/\\\//\//g' | sed -n 's/.*"instrumented_url"\s*:\s*"\([^"]*\)".*/\1/p' )
 if [ -z "${URL}" ]; then
@@ -143,7 +143,7 @@ URL="${URL}?api_key=${TESTFAIRY_API_KEY}"
 
 echo "OK!"
 /bin/echo -n "Downloading instrumented APK.. "
-${CURL} -L -o ${TMP_FILENAME} -s ${URL}
+"${CURL}" -L -o "${TMP_FILENAME}" -s ${URL}
 
 if [ ! -f "${TMP_FILENAME}" ]; then
 	echo "FAILED!"
@@ -155,9 +155,9 @@ fi
 echo "OK!"
 
 /bin/echo -n "Re-signing APK file.. "
-${ZIP} -qd ${TMP_FILENAME} 'META-INF/*'
-${JARSIGNER} -keystore "${KEYSTORE}" -storepass "${STOREPASS}" -digestalg SHA1 -sigalg MD5withRSA ${TMP_FILENAME} "${ALIAS}"
-${JARSIGNER} -verify ${TMP_FILENAME} >/dev/null
+"${ZIP}" -qd "${TMP_FILENAME}" 'META-INF/*'
+"${JARSIGNER}" -keystore "${KEYSTORE}" -storepass "${STOREPASS}" -digestalg SHA1 -sigalg MD5withRSA "${TMP_FILENAME}" "${ALIAS}"
+"${JARSIGNER}" -verify "${TMP_FILENAME}" >/dev/null
 if [ $? -ne 0 ]; then
 	echo "FAILED!"
 	echo
@@ -165,13 +165,13 @@ if [ $? -ne 0 ]; then
 	exit 1
 fi
 
-${ZIPALIGN} -f 4 ${TMP_FILENAME} ${ZIPALIGNED_FILENAME}
-rm -f ${TMP_FILENAME}
+"${ZIPALIGN}" -f 4 "${TMP_FILENAME}" "${ZIPALIGNED_FILENAME}"
+rm -f "${TMP_FILENAME}"
 echo "OK!"
 
 /bin/echo -n "Uploading signed APK to TestFairy.. "
-JSON=$( ${CURL} -s ${SERVER_ENDPOINT}/api/upload-signed -F api_key=${TESTFAIRY_API_KEY} -F apk_file=@${ZIPALIGNED_FILENAME} -F testers-groups="${TESTER_GROUPS}" -F auto-update="${AUTO_UPDATE}" -F notify="${NOTIFY}")
-rm -f ${ZIPALIGNED_FILENAME}
+JSON=$( "${CURL}" -s ${SERVER_ENDPOINT}/api/upload-signed -F api_key=${TESTFAIRY_API_KEY} -F apk_file=@${ZIPALIGNED_FILENAME} -F testers-groups="${TESTER_GROUPS}" -F auto-update="${AUTO_UPDATE}" -F notify="${NOTIFY}")
+rm -f "${ZIPALIGNED_FILENAME}"
 
 URL=$( echo ${JSON} | sed 's/\\\//\//g' | sed -n 's/.*"build_url"\s*:\s*"\([^"]*\)".*/\1/p' )
 if [ -z "$URL" ]; then
