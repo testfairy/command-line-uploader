8fa478e4b721ad825a90dff0658db6149d9f6b4b
yairba <yair@bar-on.org>
2015-12-18 15:57:52 +0200

    typo

--- testfairy-upload-android-windows.bat
+++ testfairy-upload-android-windows.bat
@@ -1,7 +1,5 @@
 REM This is a sample script for uploading an Android APK from Windows.
-REM Before running this script:
-REM 1. Install Node.js from  https://nodejs.org/en/
-REM 2. Open a new command line window (cmd) and run: npm install -g testfairy-uploader
+REM Before running this script please install Node.js from  https://nodejs.org/en/ and run: npm install -g testfairy-uploader
 REM For more information watch: https://www.youtube.com/watch?v=7wg07Q7TYbA
 
 if defined JAVA_HOME goto foundJavaHome
