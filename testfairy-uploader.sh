f89509556706b7bcfaca5765ec985a1e21fb1f07
Gil Megidish <gil@megidish.net>
2016-04-21 18:14:15 +0300

    Update testfairy-uploader.sh

--- testfairy-uploader.sh
+++ testfairy-uploader.sh
@@ -66,7 +66,7 @@ fi
 verify_tools
 verify_settings
 
-IPA_FILENAME=$1
+APP_FILENAME=$1
 if [ ! -f "${APP_FILENAME}" ]; then
 	usage
 	echo "Can't find file: ${APP_FILENAME}"
@@ -76,7 +76,7 @@ fi
 # temporary file paths
 DATE=`date`
 
-/bin/echo -n "Uploading ${IPA_FILENAME} to TestFairy.. "
+/bin/echo -n "Uploading ${APP_FILENAME} to TestFairy.. "
 JSON=$( ${CURL} -s ${SERVER_ENDPOINT}/api/upload -F api_key=${TESTFAIRY_API_KEY} -F file="@${APP_FILENAME}" -F video="${VIDEO}" -F max-duration="${MAX_DURATION}" -F comment="${COMMENT}" -F testers-groups="${TESTER_GROUPS}" -F auto-update="${AUTO_UPDATE}" -F notify="${NOTIFY}" -A "TestFairy iOS Command Line Uploader ${UPLOADER_VERSION}" )
 
 URL=$( echo ${JSON} | sed 's/\\\//\//g' | sed -n 's/.*"build_url"\s*:\s*"\([^"]*\)".*/\1/p' )
